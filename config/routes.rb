a3c8ceb927622e117733bec04b51a513c6ebd90d
Jef Amirault <jef@amirault.org>
2022-08-09 12:31:07 -0400

    custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -61,4 +61,7 @@ Rails.application.routes.draw do
   get 'diagnostic_assignment', to: 'diagnostic#assignment'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
